cfb05ff602769649d6066c4ce5852864528f454e
nmorehart <nathan.morehart@gmail.com>
2015-03-02 23:28:26 -0600

    Update sql script.sql

--- sql script.sql
+++ sql script.sql
@@ -12,21 +12,3 @@ INSERT INTO `NineLenses`.`DataPoints`
 (`TotalDataPoints`)
 VALUES
 (0);
-
-/*GET DATAPOINTS DATA*/
-DELIMITER $$
-CREATE DEFINER=`nmorehart`@`%` PROCEDURE `pGetTotalDataPoints`()
-BEGIN
-  SELECT TotalDataPoints
-    FROM DataPoints;
-END$$
-DELIMITER ;
-
-/*UPDATE DATAPOINTS DATA*/
-DELIMITER $$
-CREATE DEFINER=`nmorehart`@`%` PROCEDURE `pUpdateTotalDataPoints`(IN totalDataPoints INT)
-BEGIN
-  UPDATE DataPoints
-     SET TotalDataPoints = totalDataPoints;
-END$$
-DELIMITER ;
